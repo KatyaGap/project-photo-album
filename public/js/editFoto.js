0d207d7a1bdedea5fec2cab2172f39a851bfc940
KatyaGap <katyakate22@mail.ru>
2022-06-17 10:25:45 +0300

    Private settings

--- public/js/editFoto.js
+++ public/js/editFoto.js
@@ -1,20 +1,21 @@
-
 const form = document.querySelector('#form');
-console.log(form)
+console.log(form);
 const dogList = document.querySelector('#dogList');
 const container = document.querySelector('.container');
 
 // добавление карточки
 form?.addEventListener('submit', async (e) => {
-	e.preventDefault();
-	const { id } = form.dataset;
-	const response = await fetch(`/albumCards/${id}`, {
-		method: 'post',
-		body: new FormData(form),
-	});
-	if (response.ok) {
-		const result = await response.json();
-		dogList.insertAdjacentHTML('afterbegin', `<div id='div-${result.id}' class="card" style="width: 18rem;" >
+  e.preventDefault();
+  const { id } = form.dataset;
+  const response = await fetch(`/albumCards/${id}`, {
+    method: 'post',
+    body: new FormData(form),
+  });
+  if (response.ok) {
+    const result = await response.json();
+    dogList.insertAdjacentHTML(
+      'afterbegin',
+      `<div id='div-${result.id}' class="card" style="width: 18rem;" >
 		<img src="${result.image}" class="card-img-top" alt=${result.title_photo}>
 		<div class="card-body">
 			<h5 class="card-title">${result.photo_title}</h5>
@@ -23,68 +24,70 @@ form?.addEventListener('submit', async (e) => {
 			<button data-edit=${result.id} class="btn btn-primary" type="click">edit</button>
 			<button data-delete=${result.id} class="btn btn-primary" type="click">delete</button>
 		</div>
-	</div>`);
-	}
+	</div>`
+    );
+  }
 });
 
 // удаление карточки
 container?.addEventListener('click', async (e) => {
-	e.preventDefault();
-	if (e.target.dataset.delete) {
-		const id = e.target.dataset.delete;
-		const div = document.getElementById(`div-${id}`);
-		const response = await fetch(`/card/${id}`, {
-			method: 'delete',
-		});
-		if (response.ok) {
-			console.log(response)
-			div.remove();
-		}
-	}})
+  e.preventDefault();
+  if (e.target.dataset.delete) {
+    const id = e.target.dataset.delete;
+    const div = document.getElementById(`div-${id}`);
+    const response = await fetch(`/card/${id}`, {
+      method: 'delete',
+    });
+    if (response.ok) {
+      console.log(response);
+      div.remove();
+    }
+  }
+});
 
-
-	// редактирование карточки
-	// отлавливаем нажатие на кнопку редактирования
-	container?.addEventListener('click', async (e) => {
-		e.preventDefault();
-		if (e.target.dataset.edit) {
-			// console.log('editclick')
-				const id = e.target.dataset.edit;
-				const div = document.getElementById(`div-${id}`);
-				const response = await fetch(`/card/${id}`);
-			if (response.ok) {
-				// console.log('response', response)
-				const result = await response.json();
-				div.innerHTML = `<form id="edit" name="busForm">
+// редактирование карточки
+// отлавливаем нажатие на кнопку редактирования
+container?.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target.dataset.edit) {
+    // console.log('editclick')
+    const id = e.target.dataset.edit;
+    const div = document.getElementById(`div-${id}`);
+    const response = await fetch(`/card/${id}`);
+    if (response.ok) {
+      // console.log('response', response)
+      const result = await response.json();
+      div.innerHTML = `<form id="edit" name="busForm">
 				<div class="mb-3">
 					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="newTitle" placeholder=${result.photo_title}>
 					<input type="text" class="form-control" id="exampleInputPassword1" name="newDescription" placeholder=${result.description}>
 				</div>
 				<button data-update=${id} type="click" class="btn btn-primary">edit</button>
 			</form>`;
-			}
-		}})
+    }
+  }
+});
 
 // добавляем новую редакцию карточки
-			container?.addEventListener('click', async (e) => {
-				e.preventDefault();
-				if (e.target.dataset.update) {
-				const id = e.target.dataset.update;
-				const div = document.getElementById(`div-${id}`);
-				const formEdit = document.querySelector('#edit')
-				const formData = new FormData(formEdit);
-				let data = Object.fromEntries(formData);
-				const response = await fetch(`/card/${id}`, {
-					method: 'put',
-					headers: {
-						'Content-type': 'application/json',
-					},
-					body: JSON.stringify(data),
-				})
-				if (response.ok) {
-					const result = await response.json();
-					console.log(result)
-					div.innerHTML = `<div id='div-${result.id}' class="card" style="width: 18rem;" >
+container?.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target.dataset.update) {
+    const id = e.target.dataset.update;
+    const div = document.getElementById(`div-${id}`);
+    const formEdit = document.querySelector('#edit');
+    const formData = new FormData(formEdit);
+    let data = Object.fromEntries(formData);
+    const response = await fetch(`/card/${id}`, {
+      method: 'put',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      div.innerHTML = `<div id='div-${result.id}' class="card" style="width: 18rem;" >
 					<img src="${result.image}" class="card-img-top" alt=${result.photo_title}>
 					<div class="card-body">
 						<h5 class="card-title">${result.photo_title}</h5>
@@ -93,20 +96,21 @@ container?.addEventListener('click', async (e) => {
 						<button data-edit=${result.id} class="btn btn-primary" type="click">edit</button>
 						<button data-delete=${result.id} class="btn btn-primary" type="click">delete</button>
 					</div>
-				</div>`
-				}
-			}
-		})
+				</div>`;
+    }
+  }
+});
 
-		// view more
-		container?.addEventListener('click', async (e) => {
-			e.preventDefault();
-			if (e.target.dataset.more) {
-				const id = e.target.dataset.more;
-				const div = document.getElementById(`div-${id}`);
-				const image = div.querySelector('img')
-				const response = await fetch(`/card/${id}`)
-				if (response.ok) {
-					image.classList.toggle('viewMore');
-				}
-			}});
+// view more
+container?.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target.dataset.more) {
+    const id = e.target.dataset.more;
+    const div = document.getElementById(`div-${id}`);
+    const image = div.querySelector('img');
+    const response = await fetch(`/card/${id}`);
+    if (response.ok) {
+      image.classList.toggle('viewMore');
+    }
+  }
+});
--- routes/privateRouter.js
+++ routes/privateRouter.js
@@ -13,8 +13,10 @@ router.route('/:id')
 	try {
 		const { id } = req.params;
 		console.log('id', id);
-		const newPrivatePerson = await Privates.create({ email: req.body.privateEmail, albumId: id });
-		res.json({ newPrivatePerson});
+		const newPrivatePerson = await Privates.create({ private_email: req.body.private_email, albumId: id });
+		const Album = await Albums.findOne({ where : { id: id}})
+			const userId = Album.user_id
+		res.json({ newPrivatePerson, userId });
  } catch (error) {
 		console.log(error);
 		res.json({ error });
